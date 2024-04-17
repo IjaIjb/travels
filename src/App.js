b5f8c9e8d80de8c355167937af2e46505d8e8658
Joseph Ijaola <ijabolu@gmail.com>
2024-04-17 11:59:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import teamsbg from "./assets/IATA-2.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="relative">
+    <div className="absolute object-cover w-full h-screen">
+      <img src={teamsbg} alt="" className="object-cover absolute w-full h-screen" />
+      <div className="flex flex-col gap-2 items-center justify-center px-2 h-full text-white">
+        <h4 className="text-white text-center z-10 text-[30px] md:text-[40px] font-[600]">We are Rebranding</h4>
+        <h4 className="text-white z-10 text-center text-[22px] md:text-[32px] font-[400]">To get to know more Information about us</h4>
+      <div className='px-10 z-10 bg-amber-800 py-3  rounded-lg text-white text-md'>
+<h4>Click here </h4>
+      </div>
+      </div>
+      <div className="overlay absolute inset-0 bg-black opacity-70"></div>
     </div>
+  </div>
+  
   );
 }
 
